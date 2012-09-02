dfdee7f6da4619e2d3e3c89b7f97b931718bb066
Hakan Raberg <hraberg@yahoo.se>
2012-09-03 00:11:29 +0100

    Now supporting other types than double

--- test/sleipnir/test/vector_add.clj
+++ test/sleipnir/test/vector_add.clj
@@ -4,14 +4,14 @@
 
 (def element-count 1444477)
 
-(defkernel vector-add [^double* a ^double* b ^:out ^double* c ^int num-elements]
+(defkernel vector-add [^float* a ^float* b ^:out ^float* c ^int num-elements]
   (let [^int iGID (get-global-id 0)]
     (when (< iGID num-elements)
       (aset c iGID (+ (aget a iGID) (aget b iGID))))))
 
 (defn -main [& _]
-  (let [a (double-array (repeatedly element-count #(rand 100)))
-        b (double-array (repeatedly element-count #(rand 100)))]
+  (let [a (float-array (repeatedly element-count #(rand 100)))
+        b (float-array (repeatedly element-count #(rand 100)))]
 
     (println (str device))
     (time (println (take 10 (buffer-seq
@@ -21,7 +21,7 @@
 
     (println "Clojure")
     (time (println (take 10 (loop [i 0
-                                   acc (double-array element-count)]
+                                   acc (float-array element-count)]
                               (if (= i element-count)
                                 acc
                                 (do
