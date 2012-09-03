aa33d08c2e9f6bbb1119c54139c4b65a9166515d
Hakan Raberg <hraberg@yahoo.se>
2012-09-03 12:09:20 +0100

    Looping in the test, tidying

--- test/sleipnir/test/vector_add.clj
+++ test/sleipnir/test/vector_add.clj
@@ -1,6 +1,5 @@
 (ns sleipnir.test.vector-add
-  (use [sleipnir.core :only (defkernel run-kernel buffer-seq device)])
-  (import [com.jogamp.common.nio Buffers]))
+  (use [sleipnir.core :only (defkernel device)]))
 
 (def element-count 1444477)
 
@@ -10,18 +9,21 @@
       (aset c iGID (+ (aget a iGID) (aget b iGID))))))
 
 (defn -main [& _]
-  (let [a (float-array (repeatedly element-count #(rand 100)))
-        b (float-array (repeatedly element-count #(rand 100)))]
+  (let [a (float-array element-count (repeatedly #(rand 100)))
+        b (float-array element-count (repeatedly #(rand 100)))]
 
-    (println (str device))
-    (time (println (->> (vector-add a b :c element-count)
-                        :c (take 10))))
+    (dotimes [n 3]
+      (println)
 
-    (println "Clojure")
-    (time (println (take 10 (loop [i 0
-                                   acc (float-array element-count)]
-                              (if (= i element-count)
-                                acc
-                                (do
-                                  (aset acc i (+ (aget a i) (aget b i)))
-                                  (recur (inc i) acc)))))))))
+      (println (str device))
+      (time (println (->> (vector-add a b :c element-count)
+                          :c (take 10))))
+
+      (println "Clojure" (clojure-version))
+      (time (println (take 10 (loop [i 0
+                                     acc (float-array element-count)]
+                                (if (= i element-count)
+                                  acc
+                                  (do
+                                    (aset acc i (+ (aget a i) (aget b i)))
+                                    (recur (inc i) acc))))))))))
